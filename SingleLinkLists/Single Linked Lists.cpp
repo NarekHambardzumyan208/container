07cfdcb50ec8185a623fa1d4b860a9aa650ae5b4
NarekHambardzumyan208 <anerkaner10@mail.ru>
2023-08-01 23:06:56 +0400

    added implementation of single linked lists

---
+++ SingleLinkLists/Single Linked Lists.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+template<typename T = int>
+class Node {
+public:
+    Node* next;
+    T m_value;
+    Node() : next(nullptr), m_value(0) {};
+    Node(T data) : next(nullptr), m_value(data) {}
+};
+
+template<typename T = int>
+class list {
+    Node<T>* head = nullptr;
+public:
+    void add(T m_data) {
+        Node<T>* newNode = new Node<T>(m_data);
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node<T>* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void print() {
+        Node<T>* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->m_value << std::endl;
+            temp = temp->next;
+        }
+    }
+};
+
+int main() {
+    list<int> first;
+    first.add(1);
+    first.add(3);
+    first.add(4);
+    first.add(7);
+    first.print();
+
+    return 0;
+}
